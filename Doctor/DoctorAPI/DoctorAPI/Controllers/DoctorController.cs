3ae83ec5337d5578896099dcdd06dd9c9b7e9c90
kavyaC510 <218kavya@gmail.com>
2024-02-10 13:18:31 -0600

    Doctor API

---
+++ Doctor/DoctorAPI/DoctorAPI/Controllers/DoctorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DoctorAPI.Model;
+using DoctorAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+namespace DoctorAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DoctorController : Controller
+    {
+        [HttpGet(Name = "GetDoctor")]
+        public IEnumerable<Doctor> GetDoctors()
+        {
+            DoctorRepository docRepo = new DoctorRepository();
+            return docRepo.GetDoctors();
+        }
+        [HttpPost(Name = "Add")]
+        public void AddDoctor(Doctor doc)
+        {
+            DoctorRepository docRepo = new DoctorRepository();
+            docRepo.AddDoctor(doc);
+        }
+
+        [HttpPut(Name = "Edit")]
+        public void EditDoctor(Doctor doc)
+        {
+            DoctorRepository docRepo = new DoctorRepository();
+            docRepo.UpdateDoctor(doc);
+        }
+        [HttpGet("{id:int}")]
+        public Doctor GetDoctor(int id)
+        {
+            DoctorRepository docRepo = new DoctorRepository();
+            return docRepo.GetDoctor(id);
+
+        }
+    }
+}
---
+++ Doctor/DoctorAPI/DoctorAPI/Repository/DoctorRepository.cs
@@ -0,0 +1,87 @@
+﻿using DoctorAPI.Model;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace DoctorAPI.Repository
+{
+    public class DoctorRepository
+    {
+        internal void AddDoctor(Doctor doc)
+        {
+            //call Add Employee Stored procedure
+            var objBuilder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+            IConfiguration conManager = objBuilder.Build();
+            string connectionString = conManager.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String storedProcedureName = "InsertDoctor";
+                var parms1 = new DynamicParameters();
+                parms1.Add("Doctor_ID", doc.Doctor_ID);
+                parms1.Add("FirstName", doc.FirstName);
+                parms1.Add("LastName", doc.LastName);
+                parms1.Add("Speciality", doc.Speciality);
+                connection.ExecuteScalar(storedProcedureName, parms1, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        internal Doctor GetDoctor(int id)
+        {
+            var objBuilder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+            IConfiguration conManager = objBuilder.Build();
+            string connectionString = conManager.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String storedProcedureName = "GetDoctor";
+                var parms1 = new DynamicParameters();
+                parms1.Add("Doctor_ID", id);
+                Doctor doc = connection.QueryFirstOrDefault<Doctor>(storedProcedureName, parms1, commandType: CommandType.StoredProcedure);
+                return doc;
+            }
+        }
+
+        internal IEnumerable<Doctor> GetDoctors()
+        {
+            var objBuilder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+            IConfiguration conManager = objBuilder.Build();
+            string connectionString = conManager.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String storedProcedureName = "GetAllDoctor";
+                IEnumerable<Doctor> employees = connection.Query<Doctor>(storedProcedureName, commandType: CommandType.StoredProcedure);
+                return employees;
+            }
+        }
+
+        internal void UpdateDoctor(Doctor doc)
+        {
+            var objBuilder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+            IConfiguration conManager = objBuilder.Build();
+            string connectionString = conManager.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String storedProcedureName = "UpdateDoctor";
+                var parms1 = new DynamicParameters();
+                parms1.Add("Doctor_ID", doc.Doctor_ID);
+                parms1.Add("FirstName", doc.FirstName);
+                parms1.Add("LastName", doc.LastName);
+                parms1.Add("Speciality", doc.Speciality);
+                connection.ExecuteScalar(storedProcedureName, parms1, commandType: System.Data.CommandType.StoredProcedure);
+
+
+            }
+        }
+    }
+}
