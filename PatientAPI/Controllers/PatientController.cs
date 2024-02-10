6c3c050d2d34f10fce2fecc2190486280063c950
kavyaC510 <218kavya@gmail.com>
2024-02-10 12:21:19 -0600

    Patient done

--- PatientAPI/Controllers/PatientController.cs
+++ PatientAPI/Controllers/PatientController.cs
@@ -11,12 +11,39 @@ namespace PatientAPI.Controllers
     [Route("[controller]")]
     public class PatientController : Controller
     {
+        [HttpGet(Name = "GetPatient")]
+        public IEnumerable<Patient> GetPatient()
+        {
+            PatientRepository patRepo = new PatientRepository();
+            return patRepo.GetPatients();
+        }
 
         [HttpPost(Name = "Add")]
         public void AddPatient(Patient pat)
         {
             PatientRepository patRepo = new PatientRepository();
             patRepo.AddPatient(pat);
+        }
+        [HttpPut(Name = "Edit")]
+        public void EditPatient(Patient pat)
+        {
+            PatientRepository patRepo = new PatientRepository();
+            patRepo.UpdatePatient(pat);
+        }
+
+        [HttpDelete(Name = "Delete")]
+        public void DeletePatient(int id)
+        {
+            PatientRepository patRepo = new PatientRepository();
+            patRepo.DeletePatient(id);
+        }
+
+        [HttpGet("{id:int}")]
+        public Patient GetEmployee(int id)
+        {
+            PatientRepository patRepo = new PatientRepository();
+            return patRepo.GetPatient(id);
+
         }
     }
 }
