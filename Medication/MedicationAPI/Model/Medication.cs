77fcbcbb81bc2fcabb021d4b94a2f92800125e3f
kavyaC510 <218kavya@gmail.com>
2024-02-10 13:46:01 -0600

    Medication API

---
+++ Medication/MedicationAPI/Model/Medication.cs
@@ -0,0 +1,11 @@
+﻿namespace MedicationAPI.Model
+{
+    public class Medication
+    {
+        public int Medication_ID {  get; set; }
+        public int Patient_ID { get; set; }
+        public string Name { get; set; }
+        public int Frequency { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
